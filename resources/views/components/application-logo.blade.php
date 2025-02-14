b03775407a6714795b71595ab22483f5362ee1db
andrnnnn <andrianjack1@gmail.com>
2025-02-14 22:22:33 +0700

    fix(logo): adjust brand logo size for better appearance

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,38 @@
-<div class="text-center text-3xl font-extrabold text-black tracking-wide">
-    Sistem Laporan Pajak Penghasilan  
-    <br>
-    Kantor Konsultan Pajak Suwandi Sudarsono & Rekan
-</div>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" width="200" height="200" viewBox="0 0 375 375" {{ $attributes }}>
+    <defs>
+    <clipPath id="clip_0">
+    <path transform="matrix(.24,0,0,.24,0,0)" d="M0 0H1562.5V1562.5H0Z" clip-rule="evenodd"/>
+    </clipPath>
+    <clipPath id="clip_1">
+    <path transform="matrix(.24,0,0,.24,0,0)" d="M0 0H1562.5V1562.5H0Z" clip-rule="evenodd"/>
+    </clipPath>
+    <clipPath id="clip_2">
+    <path transform="matrix(.24,0,0,.24,0,0)" d="M0 0H1562.5V1562.5H0Z" clip-rule="evenodd"/>
+    </clipPath>
+    <clipPath id="clip_3">
+    <path transform="matrix(.24,0,0,.24,0,0)" d="M72.01337 718.02749H1472.7542V843.23837H72.01337Z" clip-rule="evenodd"/>
+    </clipPath>
+    <clipPath id="clip_4">
+    <path transform="matrix(.24,0,0,.24,0,0)" d="M0 0H1562.5V1562.5H0Z" clip-rule="evenodd"/>
+    </clipPath>
+    </defs>
+    <g clip-path="url(#clip_0)">
+    <path transform="matrix(.75,0,0,.75,0,0)" d="M0 0H500V500H0Z" fill="#ffffff"/>
+    </g>
+    <g clip-path="url(#clip_1)">
+    <path transform="matrix(.75,0,0,.75,0,0)" d="M0 0H500V500H0Z" fill="#fafafa"/>
+    </g>
+    <g clip-path="url(#clip_2)">
+    <text xml:space="preserve" transform="matrix(.73829069 0 0 .73829069 96.04478 65.44159)" font-size="231.67" font-family="BodoniFLF"><tspan y="239" x="55.515626">S</tspan></text>
+    <text xml:space="preserve" transform="matrix(.75 0 0 .75 26.558918 65.17033)" font-size="231.67" font-family="BodoniFLF"><tspan y="239" x="16.859375">S</tspan></text>
+    <text xml:space="preserve" transform="matrix(.75 0 0 .75 224.98926 61.91905)" font-size="231.67" font-family="BodoniFLF"><tspan y="239" x="6.890625">R</tspan></text>
+    </g>
+    <g clip-path="url(#clip_3)">
+    <path transform="matrix(.1173852,0,0,.1173852,17.283207,172.32659)" d="M0 0H2864V256H0Z" fill="#fafafa"/>
+    </g>
+    <g clip-path="url(#clip_4)">
+    <text xml:space="preserve" transform="matrix(.75 0 0 .75 13.916096 177.3885)" font-size="17.93" font-family="Montserrat" font-weight="bold"><tspan y="18" x="11.6875 28.088383 47.17756 72.98746 91.68234 111.129947 130.90015 141.74523 151.78382 168.1847 187.27387 207.04407 225.73896 243.87825 260.2791 280.3719 299.8195 319.9123 329.9509 347.96476 358.00337 376.14268 393.13499 411.3639 430.05879">SUWANDI SUDARSONO &#x0026; REKAN</tspan></text>
+    <text xml:space="preserve" transform="matrix(.75 0 0 .75 40.346998 270.84364)" font-size="17.33" font-family="Montserrat"><tspan y="17" x="55.640626 75.55283 97.68303 119.362689 137.61142 158.92722 176.7254 194.26367 214.27985 235.9595 248.09143 268.12495 288.1411 304.41444 324.4306">KONSULTAN PAJAK</tspan></text>
+    </g>
+    <path transform="matrix(1,0,0,-1,0,383.03999)" d="M17.283209 180.6628H353.461V210.71341H17.283209Z" fill-opacity="0"/>
+</svg>
