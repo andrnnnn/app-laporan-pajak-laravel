ec2507c83ca7a43397bdcee52102d0d44a28b2a4
andrnnnn <andrianjack1@gmail.com>
2025-02-25 15:06:09 +0700

    feat(routes): add CRUD routes for employee tax management

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PerusahaanController;
+use App\Http\Controllers\PajakKaryawanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/perusahaan/{id}/edit', [PerusahaanController::class, 'edit'])->name('perusahaan.edit');
     Route::put('/perusahaan/{id}', [PerusahaanController::class, 'update'])->name('perusahaan.update');
     Route::delete('/perusahaan/{id}', [PerusahaanController::class, 'destroy'])->name('perusahaan.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/pajak-karyawan', [PajakKaryawanController::class, 'index'])->name('pajak-karyawan.index');
+    Route::post('/pajak-karyawan', [PajakKaryawanController::class, 'store'])->name('pajak-karyawan.store');
+    Route::get('/pajak-karyawan/{id}/edit', [PajakKaryawanController::class, 'edit'])->name('pajak-karyawan.edit');
+    Route::put('/pajak-karyawan/{id}', [PajakKaryawanController::class, 'update'])->name('pajak-karyawan.update');
+    Route::delete('/pajak-karyawan/{id}', [PajakKaryawanController::class, 'destroy'])->name('pajak-karyawan.destroy');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
