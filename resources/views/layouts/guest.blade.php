8c782f400088e6f072e9cd9397cbe25464b71350
andrnnnn <andrianjack1@gmail.com>
2025-03-05 10:19:07 +0700

    feat: add button back to wellcome page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,8 +16,11 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-50 dark:bg-gray-900">
+            <a href="{{ url('/') }}" class="absolute top-6 left-6 px-4 py-2 text-black rounded-lg hover:underline">
+                Kembali
+            </a>
             <div class="text-2xl font-bold p-10 dark:text-gray-50">
-                Sistem Laporan Pajak Penghasilan
+                @yield('title', 'Sistem Laporan Pajak Penghasilan')
             </div>
             <div>
                 <a href="/">
